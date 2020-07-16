dc4670faaacba082c956d5ab0207b5a30ebf8c22
ccryoma <hid7ubokura@yahoo.co.jp>
2020-07-16 02:51:05 +0000

    Initialize repository

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,27 @@
+class MembersController < ApplicationController
+  
+  def show
+    @member = Member.find(params[:id])
+  end
+  
+  def new
+    @member = Member.new
+  end
+  
+  def create
+    @member = Member.new(member_params)
+    if @member.save
+      flash[:success] = "ラーメンログへようこそ！"
+      redirect_to @member
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def member_params
+      params.require(:member).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+end
---
+++ test/models/member_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class MemberTest < ActiveSupport::TestCase
+
+  def setup
+    @member = Member.new(name: "Example Member", email: "member@example.com",
+                        password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+    assert @member.valid?
+  end
+
+  test "name should be present" do
+    @member.name = "     "
+    assert_not @member.valid?
+  end
+
+  test "email should be present" do
+    @member.email = "     "
+    assert_not @member.valid?
+  end
+  
+  test "name should not be too long" do
+    @member.name = "a" * 51
+    assert_not @member.valid?
+  end
+
+  test "email should not be too long" do
+    @member.email = "a" * 244 + "@example.com"
+    assert_not @member.valid?
+  end
+  
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[member@example,com user_at_foo.org member.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @member.email = invalid_address
+      assert_not @member.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+  
+  test "email addresses should be unique" do
+    duplicate_member = @member.dup
+    @member.save
+    assert_not duplicate_member.valid?
+  end
+  
+  test "password should be present (nonblank)" do
+    @member.password = @member.password_confirmation = " " * 6
+    assert_not @member.valid?
+  end
+
+  test "password should have a minimum length" do
+    @member.password = @member.password_confirmation = "a" * 5
+    assert_not @member.valid?
+  end
+end
