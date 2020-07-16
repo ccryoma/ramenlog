120eb975bcdfcd2cc19f3cf69212cbc3f15da799
ccryoma <hid7ubokura@yahoo.co.jp>
2020-07-16 14:00:49 +0000

    Add password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_110146) do
+ActiveRecord::Schema.define(version: 2020_07_16_133119) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2020_07_16_110146) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_members_on_email", unique: true
   end
 
