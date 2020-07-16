db9466ec2fe762aa21bd3757cddf12aea09c4dd7
ccryoma <hid7ubokura@yahoo.co.jp>
2020-07-16 13:21:07 +0000

    heroku app replace

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'serene-hamlet-60196.herokuapp.com'
+  host = 'ramenlog.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
