629453dad0ff440248e252e23e14cfc81187b937
ccryoma <hid7ubokura@yahoo.co.jp>
2020-07-16 02:51:05 +0000

    Initialize repository

---
+++ test/integration/members_signup_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class MembersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'Member.count' do
+      post members_path, params: { member: { name:  "",
+                                         email: "member@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'members/new'
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'Member.count', 1 do
+      post members_path, params: { member: { name:  "Example User",
+                                         email: "member@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'members/show'
+  end
+end
