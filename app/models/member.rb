cd39fa6337f228960fd2b477e0cdd4e40fc2c4e4
ccryoma <hid7ubokura@yahoo.co.jp>
2020-07-16 07:54:18 +0000

    Implement basic login

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,4 +7,11 @@ class Member < ApplicationRecord
                     uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  
+  # 渡された文字列のハッシュ値を返す(fixtureパスワード用)
+  def Member.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
