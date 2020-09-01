8b1a79f8be229d03e39520f450927bbd907be2e8
水野竜馬 <mizunoryouma@mizunoryoumanoMacBook-Air.local>
2020-09-01 19:16:48 +0900

    migrate調整

--- db/migrate/20200810011050_add_geocode_to_shops.rb
+++
@@ -1,8 +0,0 @@
-class AddGeocodeToShops < ActiveRecord::Migration[6.0]
-  def change
-    change_table :shops, bulk: true do |t|
-      t.column :latitude, :float
-      t.column :longitude, :float
-    end
-  end
-end
